de67826640024b790ac8c00633100df700eb59ef
Caetano <122699973+ProgCaetano@users.noreply.github.com>
2024-02-19 14:31:20 -0300

    Script
    Atualização do botão e adição das tarefas com botão de deletar.

--- script/script.js
+++ script/script.js
@@ -1,25 +1,49 @@
 let input = document.getElementById('inputTarefa');
 let btnAdd = document.getElementById('btn-add');
+let contador = 0;
 
 function addTarefa(){
     //PEGAR O VALO DIGITADO NO INPUT
     let valorInput = input.value;
-
+  
     //SE O VALOR NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
     if((valorInput!=="") && (valorInput!==null) && (valorInput!==undefined)){
-        let novoItem = `<div class="item">
-        <div class="item-icone">
+
+    //adicionar um contador para deletar
+    ++contador;
+
+
+        let novoItem  = `<div id="${contador}" class="item">
+        <div  class="item-icone">
             <i class="mdi mdi-circle-outline"></i>
         </div>
         <div class="item-nome">
-            texte de Tarefa
+            ${valorInput}
         </div>
         <div class="item-botao">
-            <button class="delete"><i class="mdi mdi-delete"></i>Deletar</button>
+            <button onclick="deletar(${contador})" class="delete"><i class="mdi mdi-delete"></i>Deletar</button>
         </div>
     </div>`;
 
-    main.innerHTML += novoItem;
+     //adicionar novo item no main
+        document.getElementById('ilista').innerHTML += novoItem;
+
+    //zerar os campinhos
+        input.value = ""
+    //deixar o campo input focado
+        input.focus();
+
+   
 
     } 
 }
+
+//deletar o item da lista
+function deletar(id){
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+
+
+
